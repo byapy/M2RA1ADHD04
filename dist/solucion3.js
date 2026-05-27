44bf35c0c3c9226cb1500cac186d4c185b966557
ByapY <bryanyaquian@gmail.com>
2026-05-27 08:35:14 -0600

    Problema 3

--- dist/solucion3.js
+++ dist/solucion3.js
@@ -2,3 +2,13 @@
 const resultadoDiv = document.getElementById("divResultado");
 const numerito = document.getElementById("numParImpar");
 const botoncito = document.getElementById("btnParImpar");
+function NumeroParOimpar() {
+    let resultado = parseFloat(numerito.value);
+    if (resultado % 2 === 0) {
+        resultadoDiv.innerHTML = "El numero es par";
+    }
+    else {
+        resultadoDiv.innerHTML = "El numero es impar";
+    }
+}
+botoncito.addEventListener("click", NumeroParOimpar);
--- src/solucion3.ts
+++ src/solucion3.ts
@@ -1,3 +1,16 @@
 const resultadoDiv = document.getElementById("divResultado") as HTMLDivElement;
 const numerito = document.getElementById("numParImpar") as HTMLInputElement;
 const botoncito = document.getElementById("btnParImpar") as HTMLButtonElement;
+
+function NumeroParOimpar(){
+
+    let resultado:number = parseFloat(numerito.value);
+    if(resultado % 2 === 0) {
+        resultadoDiv.innerHTML = "El numero es par";
+    }
+    else{
+        resultadoDiv.innerHTML = "El numero es impar";
+    }
+}
+
+botoncito.addEventListener("click", NumeroParOimpar);
